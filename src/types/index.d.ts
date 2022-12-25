32facf2252c8d6dbaa6102d5ce0288a1f5b19289
josemiguel02 <josemidev24@gmail.com>
2022-12-24 22:38:34 -0500

    added types

---
+++ src/types/index.d.ts
@@ -0,0 +1,94 @@
+/* eslint-disable no-unused-vars */
+import { FC, PropsWithChildren, Dispatch, SetStateAction } from 'react'
+
+declare global {
+  type FCC<P = {}> = FC<PropsWithChildren<P>>
+
+  interface UserFormData {
+    identification: string
+    password: string
+  }
+
+  interface UserPayload {}
+
+  interface IUser {
+    id: number
+    name: string
+    surname: string
+    identification: string
+    position: string
+    adress: string
+    phoneNumber: string
+    phoneModel: string
+    phoneOperator: string
+    qualification: number | null
+    quizFinished: number
+  }
+
+  interface IAdmin extends Pick<IUser, 'id' | 'name' | 'surname' | 'identification'> {
+    role: string
+  }
+
+  interface IOpciones {
+    id: string
+    nombre?: string
+    img?: string
+  }
+
+  interface IPreguntas {
+    id: string
+    enunciado: string
+    opciones: IOpciones[] /* | string[] */
+    puntaje: number
+    respuesta?: number
+    tipo: 'seleccion' | 'tipeo'
+  }
+
+  interface IAdministrador {
+    id: number
+    nombre: string
+    apellido: string
+    cedula: string
+    rol: string
+  }
+
+  interface IUsuario {
+    id: number
+    nombre: string
+    apellido: string
+    cedula: string
+    direccion: string
+    celular: string
+    modelo: string
+    operadora: string
+    cargo: string
+    calificacion: number | null
+    examen_terminado: number
+    examen_id: number
+    fecha_examen: Date | null
+  }
+
+  interface IExamen {
+    id: number
+    cargo: string
+    preguntas: IPreguntas[]
+  }
+
+  type UsuarioDTO = Omit<IUsuario, 'id' | 'cargo'> & {
+    examen_id: number
+  }
+
+  type AdminDTO = Omit<IAdministrador, 'id' | 'rol'> & {
+    rol_id: number
+  }
+
+  type ExamenDTO = Omit<IExamen, 'id'>
+
+  type ReportType = {
+    position: string
+    dateFrom: string
+    dateTo: string
+  }
+}
+
+export {}
