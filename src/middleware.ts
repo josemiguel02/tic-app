3301d0ce27309b41429e65ed03c1f1b476488835
josemiguel02 <josemidev24@gmail.com>
2022-12-24 22:41:38 -0500

    added middleware for the app

---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+import { NextResponse, type NextRequest } from 'next/server'
+import { jwtVerify } from 'jose'
+
+const JWT_SECRET = process.env.JWT_SECRET_SEED!
+
+export const middleware = async (req: NextRequest) => {
+  const token = req.cookies.get('auth-token')?.value
+
+  if (!token) {
+    return NextResponse.redirect(new URL('/', req.url))
+  }
+
+  try {
+    await jwtVerify(token, new TextEncoder().encode(JWT_SECRET))
+    return NextResponse.next()
+  } catch (e) {
+    return NextResponse.redirect(new URL('/', req.url))
+  }
+}
+
+export const config = {
+  matcher: [
+    // #Usuario Normal
+    '/inicio',
+    '/examen',
+    '/examen-terminado',
+    // ? Admin
+    '/dashboard/:path*'
+    // !APIS (todas, menos las de login user, login admin)
+    // '/api/:path*'
+  ]
+}
