090a9ca20930a0e33dcedcd2d1ca5ad2c7be1f5a
josemiguel02 <josemidev24@gmail.com>
2023-01-03 23:16:50 -0500

    updated questions component

--- src/pages/examen.tsx
+++ src/pages/examen.tsx
@@ -1,10 +1,11 @@
 import dynamic from 'next/dynamic'
 import { CircularProgress } from '@chakra-ui/react'
 import { UserLayout } from '@/layouts'
+import Questions from '@/components/Questions'
 
-const Questions = dynamic(() => import('@/components/Questions'), {
-  loading: () => <CircularProgress isIndeterminate color='primary' />
-})
+// const Questions = dynamic(() => import('@/components/Questions'), {
+//   loading: () => <CircularProgress isIndeterminate color='primary' />
+// })
 
 export { getServerSideProps } from '@/utils/user-middleware'
 
